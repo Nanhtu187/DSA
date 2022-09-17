05ca3f7b8d3567ab8e59841c9d9597cd9cc1f297
nanhtu <nguyenanhtuchy2003@gmail.com>
2022-09-17 15:36:57 +0700

    Week2.1

---
+++ Week2/src/ThreeSum.java
@@ -0,0 +1,30 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+
+public class ThreeSum {
+    public static int count(int[] a) {
+        int cnt = 0;
+        int n = a.length;
+        for(int i = 0; i < n; ++ i) {
+            int run = n - 1;
+            for(int j = i + 1; j < n; ++ j) {
+                while(run > j && a[run] > -(a[i] + a[j])) {
+                    -- run;
+                }
+                if(run > j && a[run] + a[i] + a[j] == 0) {
+                    ++ cnt;
+                }
+            }
+        }
+        return cnt;
+    }
+
+    public static void main(String[] argv) {
+        int n;
+        n = StdIn.readInt();
+        int[] a = new int[n];
+        for(int i = 0; i < n; ++ i) a[i] = StdIn.readInt();
+        StdOut.print(count(a));
+    }
+}
